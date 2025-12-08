5d9dae4f016323a867d4e75530a7dd7349bc4b34
Jaskaran Singh <jaskaran.singh58@nic.in>
2025-12-08 21:09:16 +0530

    Implement tokenizer using strtok

--- shell.c
+++ shell.c
@@ -3,9 +3,31 @@
 #include <string.h> // Correct header for strcmp
 
 #define MAX_CMD_LEN 1024
+#define MAX_ARGS 64
+
+// Takes a raw string "line" and fills the "args" array with pointers to tokens
+void parse_input(char *line, char **args) {
+    int i = 0;
+
+    // Get the first token split by Space, Tab, and Newline
+    args[i] = strtok(line, " \t\n");
+
+    // Loop to get the rest
+    while (args[i] != NULL && i < MAX_ARGS - 1) {
+        i++;
+        // why NULL here? so that strtok can run from where it last left
+        args[i] = strtok(NULL, " \t\n"); 
+    }
+    
+    // IMPORTANT: The list of arguments must end with a NULL pointer so execvp knows where to stop reading.
+    // This line is so that if there are more than MAX_ARGS argument we still exit from loop and the execvp looks
+    // for null so we replace the last argument with NULL
+    args[i] = NULL; 
+}
 
 int main() {
     char command[MAX_CMD_LEN];
+    char *args[MAX_ARGS]; // Array to hold the parsed tokens
 
     while (1) {
         printf("myshell> ");
@@ -22,6 +44,16 @@ int main() {
         if (strcmp(command, "exit") == 0) {
             break;
         }
+
+        // parsing begins
+        parse_input(command, args);
+
+        // Debugging parser temporarily
+        printf("Parsed commands:\n");
+        for (int j = 0; args[j] != NULL; j++) {
+            printf("  Arg[%d]: '%s'\n", j, args[j]);
+        }
+        printf("----------------------\n");
     }
     
     return 0;
